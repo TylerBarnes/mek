64e9f8e76ea8e7127bd8af74ca92286769efc94b
Tyler Barnes <tylerdbarnes@gmail.com>
2022-11-23 17:29:24 -0800

    make properties private

--- packages/finite-signal/src/mekk.ts
+++ packages/finite-signal/src/mekk.ts
@@ -2,9 +2,8 @@ const addState = Symbol("addState")
 const addName = Symbol("addName")
 const reset = Symbol("reset")
 const initializeState = Symbol("initializeState")
-const runLifeCycles = Symbol("runLifeCycles")
-const goToNextState = Symbol("goToNextState")
 const fatalError = Symbol("fatalError")
+const getMachine = Symbol(`getMachine`)
 
 type CycleFunction = (args: FunctionArgs) => Promise<any>
 type EffectHandlerDefinition = {
@@ -23,30 +22,37 @@ type StateDefinitionInput = (() => StateDefinition) | StateDefinition
 type FunctionArgs = { context: any }
 
 export class State {
-  machine: Mech
-  definition: StateDefinition
+  #machine: Mech
+  #definition: StateDefinition
   name: string
 
-  nextState: State
-  runningLifeCycle = false
-  initialized = false
-  done = false
+  #nextState: State
+  #runningLifeCycle = false
+  #initialized = false
+  #done = false
 
-  context: any = {}
+  #context: any = {}
 
   constructor(definition: StateDefinitionInput) {
     setImmediate(() => {
-      this.definition =
+      this.#definition =
         typeof definition === `function` ? definition() : definition
 
       // so that this runs after the machine has initialized
       setImmediate(() => {
-        this.machine = this.definition.machine
-        this?.machine?.[addState]?.(this)
+        this.#machine = this.#definition.machine
+
+        if (this.#machine[addState]) {
+          this.#machine[addState](this)
+        }
       })
     })
 
     return this
+  }
+
+  get [getMachine]() {
+    return this.#machine
   }
 
   [addName](name: string) {
@@ -54,44 +60,46 @@ export class State {
   }
 
   [reset]() {
-    if (!this.initialized) return
+    if (!this.#initialized) return
 
-    this.initialized = false
-    this.done = false
-    this.nextState = null
+    this.#initialized = false
+    this.#done = false
+    this.#nextState = null
   }
 
   async [initializeState]({ context }: FunctionArgs) {
-    if (this.initialized) {
-      return this[fatalError](
+    if (this.#initialized) {
+      return this.#fatalError(
         new Error(
           `State ${this.name} has already been initialized. States can only be initialized one time. Either this is a bug or you're abusing the public api :)`
         )
       )
     } else {
-      this.initialized = true
+      this.#initialized = true
     }
 
-    this.context = context || {}
+    this.#context = context || {}
 
-    await this[runLifeCycles]()
+    await this.#runLifeCycles()
   }
 
-  private async [runLifeCycles]() {
-    if (this.done) {
-      throw new Error(
-        `State ${this.name} has already run. Cannot run life cycles again.`
+  async #runLifeCycles() {
+    if (this.#done) {
+      this.#fatalError(
+        new Error(
+          `State ${this.name} has already run. Cannot run life cycles again.`
+        )
       )
     }
 
-    if (this.runningLifeCycle) {
+    if (this.#runningLifeCycle) {
       throw new Error(`Life cycles are already running for state ${this.name}`)
     } else {
-      this.runningLifeCycle = true
+      this.#runningLifeCycle = true
     }
 
-    const lifeCycles = this.definition.life || []
-    const { context } = this
+    const lifeCycles = this.#definition.life || []
+    const context = this.#context
 
     let runReturn = {}
 
@@ -112,7 +120,7 @@ export class State {
         try {
           conditionMet = cycle.condition({ context })
         } catch (e) {
-          return this[fatalError](
+          return this.#fatalError(
             new Error(
               `Cycle condition in state ${this.name}.life[${cycleIndex}].cycle.condition threw error:\n${e.stack}`
             )
@@ -131,7 +139,7 @@ export class State {
         (typeof cycle.run?.effectHandler !== `function` ||
           cycle.run?.type !== `EffectHandler`)
       ) {
-        return this[fatalError](
+        return this.#fatalError(
           new Error(
             `Life cycle run must be an effect function. State: ${this.name}. @TODO add docs link`
           )
@@ -142,7 +150,7 @@ export class State {
         try {
           runReturn = (await cycle.run.effectHandler({ context })) || {}
         } catch (e) {
-          return this[fatalError](
+          return this.#fatalError(
             new Error(
               `Cycle "run" function in state ${this.name}.life[${cycleIndex}].cycle.run threw error:\n${e.stack}`
             )
@@ -160,9 +168,9 @@ export class State {
 
       if (thenGoToExists && typeof cycle.thenGoTo === `function`) {
         try {
-          this.nextState = cycle.thenGoTo()
+          this.#nextState = cycle.thenGoTo()
         } catch (e) {
-          return this[fatalError](
+          return this.#fatalError(
             new Error(
               `Cycle "thenGoTo" function in state ${this.name}.life[${cycleIndex}].cycle.thenGoTo threw error:\n${e.stack}`
             )
@@ -172,28 +180,28 @@ export class State {
       }
     }
 
-    this.runningLifeCycle = false
-    this[goToNextState](runReturn)
+    this.#runningLifeCycle = false
+    this.#goToNextState(runReturn)
   }
 
-  [goToNextState](context: any = {}) {
-    const machine = this.machine
+  #goToNextState(context: any = {}) {
+    const machine = this.#machine
 
-    this.done = true
+    this.#done = true
 
-    if (this.nextState) {
-      machine.transition(this.nextState, context)
+    if (this.#nextState) {
+      machine.transition(this.#nextState, context)
     } else {
       machine.stop()
     }
   }
 
-  [fatalError](error: Error) {
-    if (!this.machine) {
+  #fatalError(error: Error) {
+    if (!this.#machine) {
       throw error
     }
 
-    return this.machine[fatalError](error)
+    return this.#machine[fatalError](error)
   }
 }
 
@@ -214,29 +222,29 @@ type MechDefinitionInput = (() => MechDefinition) | MechDefinition
 export class Mech {
   name?: string
 
-  initialized = false
+  #initialized = false
   status: `stopped` | `running` = `stopped`
 
-  initialState: State
-  currentState: State
+  #initialState: State
+  #currentState: State
 
-  transitionCount = 0
-  transitionCountCheckpoint = 0
-  lastTransitionCountCheckTime = Date.now()
+  #transitionCount = 0
+  #transitionCountCheckpoint = 0
+  #lastTransitionCountCheckTime = Date.now()
 
   states: State[] = []
-  definition: MechDefinition
+  #definition: MechDefinition
 
-  onStartPromise: Promise<undefined>
-  resolveOnStart: typeof Promise.resolve
-  rejectOnStart: typeof Promise.reject
-  awaitingStartPromise: boolean = false
+  #onStartPromise: Promise<undefined>
+  #resolveOnStart: typeof Promise.resolve
+  #rejectOnStart: typeof Promise.reject
+  #awaitingStartPromise: boolean = false
 
-  onStopPromise: Promise<undefined>
-  resolveOnStop: typeof Promise.resolve
-  rejectOnStop: typeof Promise.reject
-  awaitingStopPromise: boolean = false
-  stopInterval: NodeJS.Timeout
+  #onStopPromise: Promise<undefined>
+  #resolveOnStop: typeof Promise.resolve
+  #rejectOnStop: typeof Promise.reject
+  #awaitingStopPromise: boolean = false
+  #stopInterval: NodeJS.Timeout
 
   constructor(machineDef: MechDefinitionInput) {
     this.createLifeCyclePromises()
@@ -263,67 +271,72 @@ export class Mech {
   }
 
   private createLifeCyclePromises() {
-    if (!this.awaitingStartPromise) {
-      this.awaitingStartPromise = false
+    if (!this.#awaitingStartPromise) {
+      this.#awaitingStartPromise = false
 
-      this.onStartPromise = new Promise((res, rej) => {
+      this.#onStartPromise = new Promise((res, rej) => {
         // @ts-ignore
-        this.resolveOnStart = res
+        this.#resolveOnStart = res
         // @ts-ignore
-        this.rejectOnStart = (args) => {
-          if (this.stopInterval) {
-            clearInterval(this.stopInterval)
+        this.#rejectOnStart = (args) => {
+          if (this.#stopInterval) {
+            clearInterval(this.#stopInterval)
           }
           rej(args)
         }
       })
     }
 
-    if (!this.awaitingStopPromise) {
-      if (this.stopInterval) {
-        clearInterval(this.stopInterval)
+    if (!this.#awaitingStopPromise) {
+      if (this.#stopInterval) {
+        clearInterval(this.#stopInterval)
       }
 
-      this.awaitingStopPromise = false
+      this.#awaitingStopPromise = false
       // this prevents node process from closing until machine.stop() is called
-      this.stopInterval = setInterval(() => {}, 1_000_000)
+      this.#stopInterval = setInterval(() => {}, 1_000_000)
 
-      this.onStopPromise = new Promise((res, rej) => {
+      this.#onStopPromise = new Promise((res, rej) => {
         // @ts-ignore
-        this.resolveOnStop = (args) => {
-          clearInterval(this.stopInterval)
+        this.#resolveOnStop = (args) => {
+          clearInterval(this.#stopInterval)
           // @ts-ignore
           res(args)
         }
 
         // @ts-ignore
-        this.rejectOnStop = (args) => {
-          clearInterval(this.stopInterval)
+        this.#rejectOnStop = (args) => {
+          clearInterval(this.#stopInterval)
           rej(args)
         }
       })
     }
   }
 
-  async [fatalError](error: Error) {
-    if (typeof this.definition?.onError === `function`) {
+  // States use this to throw errors to their machine
+  [fatalError](error: Error) {
+    this.#fatalError(error)
+  }
+
+  async #fatalError(error: Error) {
+    if (typeof this.#definition?.onError === `function`) {
       await this.stop()
-      await this.definition.onError(error)
+      await this.#definition.onError(error)
       return false
     }
 
-    const awaitingOn = this.awaitingStopPromise || this.awaitingStartPromise
+    const awaitingOn = this.#awaitingStopPromise || this.#awaitingStartPromise
 
     if (awaitingOn) {
       error.message = `${error.message}\n\nMachine errored (see message above). An error was thrown in machine.onStart() and machine.onStop() promises. If you'd prefer these promises resolve, you can handle errors yourself by adding an onError function to your machine definition. @TODO add docs link.`
     }
 
-    if (this.awaitingStartPromise) {
-      this.rejectOnStart(error)
+    if (this.#awaitingStartPromise) {
+      this.#rejectOnStart(error)
     }
 
-    if (this.awaitingStopPromise) {
-      this.rejectOnStop(error)
+    if (this.#awaitingStopPromise) {
+      this.#rejectOnStop(error)
     }
 
     if (awaitingOn) {
@@ -337,8 +350,8 @@ export class Mech {
   }
 
   [addState](state: State) {
-    if (this.initialized) {
-      return this[fatalError](
+    if (this.#initialized) {
+      return this.#fatalError(
         new Error(
           "Machine is already running. You cannot add a state after a machine has started."
         )
@@ -349,15 +362,15 @@ export class Mech {
   }
 
   initialize() {
-    for (const [stateName, state] of Object.entries(this.definition.states)) {
-      if (typeof state?.machine === `undefined`) {
-        return this[fatalError](
+    for (const [stateName, state] of Object.entries(this.#definition.states)) {
+      if (typeof state[getMachine] === `undefined`) {
+        return this.#fatalError(
           new Error(
             `State "${stateName}" does not have a machine defined in its state definition. @TODO add docs link`
           )
         )
-      } else if (state.machine !== this) {
-        return this[fatalError](
+      } else if (state[getMachine] !== this) {
+        return this.#fatalError(
           new Error(
             `State "${stateName}" was defined on a different machine. All states must be added to this machine's definition, and this machine must be added to their definition. @TODO add docs link.`
           )
@@ -368,7 +381,7 @@ export class Mech {
         stateName.charAt(0) === stateName.charAt(0).toUpperCase()
 
       if (!nameIsCapitalized) {
-        this[fatalError](
+        this.#fatalError(
           new Error(`State names must be capitalized. State: ${stateName}`)
         )
 
@@ -380,8 +393,8 @@ export class Mech {
 
     // states add themselves here. lets make sure they exist on this machine
     for (const state of this.states) {
-      if (!this.definition.states[state.name]) {
-        return this[fatalError](
+      if (!this.#definition.states[state.name]) {
+        return this.#fatalError(
           new Error(
             `State "${state.name}" does not exist in this machines definition. @TODO add docs link`
           )
@@ -390,7 +403,7 @@ export class Mech {
     }
 
     this.setInitialStateDefinition()
-    this.initialized = true
+    this.#initialized = true
 
     return true
   }
@@ -400,7 +413,7 @@ export class Mech {
       inputDefinition instanceof Object && !Array.isArray(inputDefinition)
 
     if (typeof inputDefinition !== `function` && !isObjectDef) {
-      this[fatalError](
+      this.#fatalError(
         new Error(
           `Machine definition must be a function or and object. @TODO add link to docs`
         )
@@ -409,32 +422,32 @@ export class Mech {
       return
     }
     try {
-      this.definition =
+      this.#definition =
         typeof inputDefinition === `function`
           ? inputDefinition()
           : inputDefinition
 
-      if (this.definition.name) {
-        this.name = this.definition.name
+      if (this.#definition.name) {
+        this.name = this.#definition.name
       }
     } catch (e) {
-      this[fatalError](new Error(`Machine definition threw error:\n${e.stack}`))
+      this.#fatalError(new Error(`Machine definition threw error:\n${e.stack}`))
     }
   }
 
   private setInitialStateDefinition() {
-    if (this.initialState) {
+    if (this.#initialState) {
       return
     }
 
-    if (this.definition.initialState instanceof State) {
-      this.initialState = this.definition.initialState
+    if (this.#definition.initialState instanceof State) {
+      this.#initialState = this.#definition.initialState
 
       return
     }
 
-    const initialStateName = Object.keys(this.definition.states)[0]
-    this.initialState = this.definition.states[initialStateName]
+    const initialStateName = Object.keys(this.#definition.states)[0]
+    this.#initialState = this.#definition.states[initialStateName]
   }
 
   async start() {
@@ -443,22 +456,22 @@ export class Mech {
     }
 
     this.status = `running`
-    this.resolveOnStart()
+    this.#resolveOnStart()
 
     // recreate lifecycle promises so that we can start again later
     this.createLifeCyclePromises()
 
-    if (this.initialState) {
-      this.transition(this.initialState, {})
+    if (this.#initialState) {
+      this.transition(this.#initialState, {})
     } else {
       this.stop()
     }
   }
 
   async stop() {
-    this.resolveOnStop()
+    this.#resolveOnStop()
     // incase we stop before we start, resolve the start promise so code can continue
-    this.resolveOnStart()
+    this.#resolveOnStart()
 
     this.status = `stopped`
   }
@@ -468,14 +481,14 @@ export class Mech {
       return
     }
 
-    if (nextState.machine !== this) {
-      const wrongMachineName = nextState.machine?.name
+    if (nextState[getMachine] !== this) {
+      const wrongMachineName = nextState[getMachine]?.name
       const nextStateName = nextState.name
 
-      return this[fatalError](
+      return this.#fatalError(
         new Error(
           `State "${
-            this.currentState.name
+            this.#currentState.name
           }" attempted to transition to a state that was defined on a different machine${
             nextStateName
               ? ` (State "${nextStateName}"${
@@ -487,29 +500,29 @@ export class Mech {
       )
     }
 
-    const previousState = this.currentState
+    const previousState = this.#currentState
 
-    this.currentState = nextState
+    this.#currentState = nextState
 
     // reset the state so it can be used again if it was used before
-    this.currentState[reset]()
+    this.#currentState[reset]()
 
     // this.onTransitionListeners.forEach((listener) =>
     //   listener({ currentState: this.currentState, previousState })
     // )
 
-    this.transitionCount++
+    this.#transitionCount++
 
-    if (this.transitionCount % 1000 === 0) {
+    if (this.#transitionCount % 1000 === 0) {
       const shouldContinue = this.checkForInfiniteTransitionLoop()
 
       if (shouldContinue) {
         setImmediate(() => {
-          this.currentState[initializeState]({ context })
+          this.#currentState[initializeState]({ context })
         })
       }
     } else {
-      this.currentState[initializeState]({ context })
+      this.#currentState[initializeState]({ context })
     }
   }
 
@@ -517,41 +530,43 @@ export class Mech {
     const now = Date.now()
 
     const lastCheckWasOver1Second =
-      now - this.lastTransitionCountCheckTime > 1000
+      now - this.#lastTransitionCountCheckTime > 1000
 
     const lastCheckWasUnder3Seconds =
-      now - this.lastTransitionCountCheckTime < 3000
+      now - this.#lastTransitionCountCheckTime < 3000
 
     const shouldCheck = lastCheckWasOver1Second && lastCheckWasUnder3Seconds
 
     const maxTransitionsPerSecond =
-      this.definition?.options?.maxTransitionsPerSecond || 1000000
+      this.#definition?.options?.maxTransitionsPerSecond || 1000000
 
     const exceededMaxTransitionsPerSecond =
-      this.transitionCount - this.transitionCountCheckpoint >
+      this.#transitionCount - this.#transitionCountCheckpoint >
       maxTransitionsPerSecond
 
     if (shouldCheck && exceededMaxTransitionsPerSecond) {
-      return this[fatalError](
+      return this.#fatalError(
         new Error(
-          `Exceeded max transitions per second. You may have an infinite state transition loop happening. Total transitions: ${this.transitionCount}, transitions in the last second: ${this.transitionCountCheckpoint}`
+          `Exceeded max transitions per second. You may have an infinite state transition loop happening. Total transitions: ${
+            this.#transitionCount
+          }, transitions in the last second: ${this.#transitionCountCheckpoint}`
         )
       )
     } else if (shouldCheck) {
-      this.transitionCountCheckpoint = this.transitionCount
+      this.#transitionCountCheckpoint = this.#transitionCount
     }
 
     return true
   }
 
   public onStart(callback?: () => Promise<void> | void) {
-    this.awaitingStartPromise = true
-    return this.onStartPromise.then(callback || (() => {}))
+    this.#awaitingStartPromise = true
+    return this.#onStartPromise.then(callback || (() => {}))
   }
 
   public onStop(callback?: () => Promise<void> | void) {
-    this.awaitingStopPromise = true
-    return this.onStopPromise.then(callback || (() => {}))
+    this.#awaitingStopPromise = true
+    return this.#onStopPromise.then(callback || (() => {}))
   }
 }
 
