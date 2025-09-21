bad627451f0f69a90b633870bbb3cca8a3141248
Tyler Barnes <tylerdbarnes@gmail.com>
2025-09-21 14:21:26 -0700

    formatting

--- src/machine.spec.ts
+++ src/machine.spec.ts
@@ -294,15 +294,15 @@ describe(`create.machine`, () => {
 
     const endTime = Date.now() - startTime
     console.log({ endTime })
-expect(endTime).toBeLessThan(5000)
+    expect(endTime).toBeLessThan(5000)
   })
 
   test.todo(
-    `machines have storage that can be accessed/mutated in states and signals`
+    `machines have storage that can be accessed/mutated in states and signals`,
   )
 
   test.todo(
-    `states have storage that is only accessible (from the state or from signals) while in that state, and persists across transitions to and from a state`
+    `states have storage that is only accessible (from the state or from signals) while in that state, and persists across transitions to and from a state`,
   )
 
   test.todo(`cycle ifs and effects can access state and machine storage`)
@@ -310,66 +310,66 @@ expect(endTime).toBeLessThan(5000)
   test.todo(`machines can be linked together and communicate via signals`)
 
   test.todo(
-    `machine and state definitions are as static as possible and can't be changed after creation. For example any thenGoTo function is called when the machine is defined, not during state lifecycles.`
+    `machine and state definitions are as static as possible and can't be changed after creation. For example any thenGoTo function is called when the machine is defined, not during state lifecycles.`,
   )
 
   test.todo(
-    `states can fork into multiple simultaneous state trees with cycle({ thenGoTo: [StateOne, StateTwo, Etc]})`
+    `states can fork into multiple simultaneous state trees with cycle({ thenGoTo: [StateOne, StateTwo, Etc]})`,
   )
 
   test.todo(
-    `state definitions are static and are only processed when they are defined the first time. for example ifs in thenGoTo will only run the first time (to discourage using unmappable conditionals inside thenGoTo)`
+    `state definitions are static and are only processed when they are defined the first time. for example ifs in thenGoTo will only run the first time (to discourage using unmappable conditionals inside thenGoTo)`,
   )
 
   test.todo(
-    `state trees can end themselves by calling cycle.end({ if: () => true })`
+    `state trees can end themselves by calling cycle.end({ if: () => true })`,
   )
 
   test.todo(
-    `states in forked state trees share the same storage per-state definition and per-machine`
+    `states in forked state trees share the same storage per-state definition and per-machine`,
   )
 
   test.todo(
-    `signals which enqueue transition requests can choose to request on the main state tree or inspect and select forked state trees to request`
+    `signals which enqueue transition requests can choose to request on the main state tree or inspect and select forked state trees to request`,
   )
 
   test.todo(
-    `signals which subscribe to transitions can choose to subscribe on the main state tree or inspect and select forked state trees to subscribe to`
+    `signals which subscribe to transitions can choose to subscribe on the main state tree or inspect and select forked state trees to subscribe to`,
   )
 
   test.todo(
-    `machines can have plugins which hook into transitions, state definitions, machine definition, and machine stop/start/error`
+    `machines can have plugins which hook into transitions, state definitions, machine definition, and machine stop/start/error`,
   )
 
   test.todo(
-    `machine plugins can mutate the machine definition during initial start up`
+    `machine plugins can mutate the machine definition during initial start up`,
   )
 
   test.todo(
-    `machine plugins can mutate the state definitions during initial start up`
+    `machine plugins can mutate the state definitions during initial start up`,
   )
 
   test.todo(
-    `machine plugins can mutate machine/state storage on machine start/stop`
+    `machine plugins can mutate machine/state storage on machine start/stop`,
   )
 
   test.todo(
-    `when process.env.NODE_ENV === 'test', machines don't automatically start until started with machine.start()`
+    `when process.env.NODE_ENV === 'test', machines don't automatically start until started with machine.start()`,
   )
 
   test.todo(
-    `machine.mergeMockDefinition() allows merging new values into the machine definition`
+    `machine.mergeMockDefinition() allows merging new values into the machine definition`,
   )
 
   test.todo(
-    `machine.mergeMockDefinition() can only be called before the machine is started`
+    `machine.mergeMockDefinition() can only be called before the machine is started`,
   )
 
   test.todo(
-    `machine.mergeMockDefinition().mergeMockState() extends State definitions`
+    `machine.mergeMockDefinition().mergeMockState() extends State definitions`,
   )
 
   test.todo(
-    `machine/state/signal do not expose methods that aren't public API's`
+    `machine/state/signal do not expose methods that aren't public API's`,
   )
 })
